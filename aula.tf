85a0188f0c7a6e60c218df36a8d11c2bab5666c5
Patrick <ticksobrinho@hotmail.com>
2022-02-02 17:14:58 -0300

    ADD: Adding VPC and Subnet

--- aula.tf
+++ aula.tf
@@ -11,7 +11,23 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "helloworld" {
-    ami = "ami-04505e74c0741db8d"
-    instance_type = "t2.micro"
+#resource "aws_instance" "helloworld" {
+#    ami = "ami-04505e74c0741db8d"
+#    instance_type = "t2.micro"
+#}
+
+resource "aws_vpc" "vpc_brq" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "VPC_legal"
+    }
+}
+
+resource "aws_subnet" "BRQ_Subrede" {
+    vpc_id = aws_vpc.vpc_brq.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+      Name = "Subrede_BRQ"
+    }
 }
