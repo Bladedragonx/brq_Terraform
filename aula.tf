430026bcb19ca3182fe11d1f68516ccf07247807
Patrick <ticksobrinho@hotmail.com>
2022-02-03 15:15:11 -0300

    add: adding Avaliability Zone in terraform file

--- aula.tf
+++ aula.tf
@@ -11,10 +11,10 @@ provider "aws" {
     region = "us-east-1"
 }
 
-#resource "aws_instance" "helloworld" {
-#    ami = "ami-04505e74c0741db8d"
-#    instance_type = "t2.micro"
-#}
+# resource "aws_instance" "helloworld" {
+#     ami = "ami-04505e74c0741db8d"
+#     instance_type = "t2.micro"
+# }
 
 resource "aws_vpc" "vpc_brq" {
     cidr_block = "10.0.0.0/16"
@@ -23,13 +23,14 @@ resource "aws_vpc" "vpc_brq" {
     }
 }
 
-# resource "aws_subnet" "BRQ_Subrede" {
-#     vpc_id = aws_vpc.vpc_brq.id
-#     cidr_block = "10.0.1.0/24"
-#     tags = {
-#       Name = "Subrede_BRQ"
-#     }
-# }
+resource "aws_subnet" "BRQ_Subrede" {
+    vpc_id = aws_vpc.vpc_brq.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "Subrede_BRQ"
+    }
+}
 
 resource "aws_internet_gateway" "BRQ_gate" {
     vpc_id = aws_vpc.vpc_brq.id
@@ -38,20 +39,20 @@ resource "aws_internet_gateway" "BRQ_gate" {
     }
 }
 
-resource "aws_route_table" "brq_route" {
-  vpc_id = aws_vpc.vpc_brq.id
+# resource "aws_route_table" "brq_route" {
+#   vpc_id = aws_vpc.vpc_brq.id
 
-  route {
-    cidr_block = "0.0.0.0/24"
-    gateway_id = aws_internet_gateway.BRQ_gate.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/24"
+#     gateway_id = aws_internet_gateway.BRQ_gate.id
+#   }
 
-  route {
-    ipv6_cidr_block        = "::/0"
-    gateway_id = aws_internet_gateway.BRQ_gate.id
-  }
+#   route {
+#     ipv6_cidr_block        = "::/0"
+#     gateway_id = aws_internet_gateway.BRQ_gate.id
+#   }
 
-  tags = {
-    Name = "Route_BRQ"
-  }
-}
+#   tags = {
+#     Name = "Route_BRQ"
+#   }
+# }
