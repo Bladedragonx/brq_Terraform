9a7e400ad4041e4b5bb1235f2e95908c0bad9f16
Patrick <ticksobrinho@hotmail.com>
2022-02-03 20:10:35 -0300

    Fix: Fixing route table from terraform file

--- aula.tf
+++ aula.tf
@@ -11,11 +11,6 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "helloworld" {
-    ami = "ami-04505e74c0741db8d"
-    instance_type = "t2.micro"
-}
-
 resource "aws_vpc" "vpc_brq" {
     cidr_block = "10.0.0.0/16"
     tags = {
@@ -23,100 +18,120 @@ resource "aws_vpc" "vpc_brq" {
     }
 }
 
+resource "aws_internet_gateway" "brq_gate" {
+    vpc_id = aws_vpc.vpc_brq.id
+    tags = {
+    	Name = "Gateway_BRQ"
+    }
+}
+
+resource "aws_route_table" "brq_route" {
+	vpc_id = aws_vpc.vpc_brq.id
+
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_internet_gateway.brq_gate.id
+	}
+
+	route {
+			ipv6_cidr_block = "::/0"
+			gateway_id = aws_internet_gateway.brq_gate.id
+	}
+
+	tags = {
+			Name = "Route_BRQ"
+	}
+}
+
 resource "aws_subnet" "brq_subrede" {
     vpc_id = aws_vpc.vpc_brq.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1a"
     tags = {
-      Name = "Subrede_BRQ"
-    }
-}
-
-resource "aws_internet_gateway" "brq_gate" {
-    vpc_id = aws_vpc.vpc_brq.id
-    tags = {
-        Name = "Gateway_BRQ"
+    	Name = "Subrede_BRQ"
     }
 }
 
 resource "aws_route_table_association" "associacao" {
-  subnet_id = aws_subnet.brq_subrede.id
-  route_table_id = aws_route_table.brq_route.id
-}
-
-resource "aws_route_table" "brq_route" {
-  vpc_id = aws_vpc.vpc_brq.id
-
-  route {
-    cidr_block = "0.0.0.0/24"
-    gateway_id = aws_internet_gateway.brq_gate.id
-  }
-
-  route {
-    ipv6_cidr_block        = "::/0"
-    gateway_id = aws_internet_gateway.brq_gate.id
-  }
-
-  tags = {
-    Name = "Route_BRQ"
-  }
+	subnet_id = aws_subnet.brq_subrede.id
+	route_table_id = aws_route_table.brq_route.id
 }
 
 resource "aws_security_group" "firewall_brq" {
-  name        = "abrir portas"
-  description = "Abrir porta 22 (SSH), 443 (HTTPS) e 80 (HTTP)"
-  vpc_id      = aws_vpc.vpc_brq.id
+	name        = "abrir portas"
+	description = "Abrir porta 22 (SSH), 443 (HTTPS) e 80 (HTTP)"
+	vpc_id      = aws_vpc.vpc_brq.id
 
-  ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+	ingress {
+		description      = "HTTPS"
+		from_port        = 443
+		to_port          = 443
+		protocol         = "tcp"
+		cidr_blocks      = ["0.0.0.0/0"]
+	}
+	ingress {
+		description      = "HTTP"
+		from_port        = 80
+		to_port          = 80
+		protocol         = "tcp"
+		cidr_blocks      = ["0.0.0.0/0"]
+	}
+	ingress {
+		description      = "SSH"
+		from_port        = 22
+		to_port          = 22
+		protocol         = "tcp"
+		cidr_blocks      = ["0.0.0.0/0"]
+	}
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+	egress {
+		from_port        = 0
+		to_port          = 0
+		protocol         = "-1"
+		cidr_blocks      = ["0.0.0.0/0"]
+		ipv6_cidr_blocks = ["::/0"]
+	}
 
-  tags = {
-    Name = "brq_firewall"
-  }
+	tags = {
+		Name = "brq_firewall"
+	}
 }
 
 resource "aws_network_interface" "interface_brq" {
-  subnet_id       = aws_subnet.brq_subrede.id
-  private_ips     = ["10.0.1.51"]
-  security_groups = [aws_security_group.firewall_brq.id]
-
-  tags = {
-    Name = "BRQ_Interface"
-  }
+	subnet_id       = aws_subnet.brq_subrede.id
+	private_ips     = ["10.0.1.51"]
+	security_groups = [aws_security_group.firewall_brq.id]
+	tags = {
+		Name = "BRQ_Interface"
+	}
 }
 
 resource "aws_eip" "brq_pub_ip" {
-  vpc = true
-  network_interface         = aws_network_interface.interface_brq.id
-  instance                  = aws_instance.helloworld.id
-  associate_with_private_ip = "10.0.1.51"
-  depends_on                = [aws_internet_gateway.brq_gate]
+	vpc                       = true
+	network_interface         = aws_network_interface.interface_brq.id
+	associate_with_private_ip = "10.0.1.51"
+	depends_on                = [aws_internet_gateway.brq_gate]
+}
+
+resource "aws_instance" "app-brq" {
+	ami               = "ami-04505e74c0741db8d"
+	instance_type     = "t2.micro"
+	availability_zone = "us-east-1a"
+	network_interface {
+		device_index         = 0
+		network_interface_id = aws_network_interface.interface_brq.id
+	}
+	user_data = <<-EOF
+		#! /bin/bash
+		sudo apt-get update -y
+		sudo apt-get install -y apache2
+		sudo systemctl start apache2
+		sudo systemctl enable apache2
+		sudo bash -c 'echo "<h1>Testando HTML na AWS via Terraform - Patrick</h1>"  > /var/www/html/index.html'
+	EOF
+	tags = {
+		Name = "Instance App Web"
+	}
+	key_name = "Minha Chave"
 }
 
